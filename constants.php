1db58778f379b06f1bec60d0eff299d84aaf1520
Jeet Naik <jeet.naik@potenzaglobalsolutions.com>
2021-06-17 10:24:23 +0530

    Initial commit

---
+++ constants.php
@@ -0,0 +1,6 @@
+<?php
+// credential
+define('RAZOR_KEY_ID', 'rzp_test_4BZUKRGd8xGmES');
+define('RAZOR_KEY_SECRET', 'lxgkFbUPQQ3VRWHSN98mBB5E');
+
+?>
---
+++ templates/footer.php
@@ -0,0 +1,13 @@
+<!-- Footer -->
+  <footer class="footer bg-light footer-bg-dark">
+    <div class="container">
+      <div class="row">
+      </div>
+    </div>
+  </footer>
+  <!-- Bootstrap core JavaScript -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
